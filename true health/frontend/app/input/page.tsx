592d93a85e6c83a65f39464a6e59df4f90e41442
muhammedSuhail <suhailkzp1@gmail.com>
2026-04-02 11:01:36 +0530

    input page started

--- true health/frontend/app/input/page.tsx
+++ true health/frontend/app/input/page.tsx
@@ -1,7 +1,12 @@
-import React from "react";
-
+import Header from "@/components/header";
 export const Page = () => {
-  return <div>page</div>;
+  return (
+    <main className="min-h-screen w-screen flex justify-center pt-10 ">
+      <section className="max-h-[80vh] w-screen sm:w-96  bg-[#131B2E] flex flex-col items-center  rounded pt-10">
+        <Header />
+      </section>
+    </main>
+  );
 };
 
 export default Page;
---
+++ true health/frontend/components/header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className=" flex flex-col items-center ">
+      <h1 className="text-white text-2xl font-bold">Scan or Enter Medicine</h1>
+      <p className="text-[#6F778E] text-xs">
+        Verify your pharmaceutical products instantly
+      </p>
+    </div>
+  );
+};
+
+export default Header;
